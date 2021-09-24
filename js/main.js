e457cadd561d7fb9cda61ccaa8dd597613d6ebbe
Federico Batistella <federicobatistella@gmail.com>
2021-09-24 17:57:40 -0300

    agregando archivos
    agrego los html y las carpetas css y js , del curso. para estar al dia

---
+++ js/main.js
@@ -0,0 +1,40 @@
+var titulo = document.querySelector('.title');
+
+
+
+
+    
+    
+
+function myfunction() {
+    
+    return titulo.style.color = 'red';
+}
+
+function myfunction2() {
+    
+    return titulo.style.color = 'lime',
+            titulo.style.fontsize = '2em';
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
